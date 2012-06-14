aa078740c359a59bb59c315a13ee41b63d925b15
Frank Schmitt <frank@qwhon.de>
2012-06-14 20:14:14 +0000

    Movies: changed title to original (Movie Title / Release Date)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def sortable(column, id)
-    title = column.titleize
+  def sortable(title, column, id)
+#title = column.titleize
 #css = column == sort_column ? "current {sort_direction}" : nil
     link_to title, {:sort => column}, :id => id
   end
