832b9af0af899c91465a5b296b9a230fbaae54de
Frank Schmitt <frank@qwhon.de>
2012-06-15 20:47:16 +0000

    Movies: CSS highlighting for column header used for sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   def sortable(title, column, id)
-#title = column.titleize
-#css_class = column == sort_column ? "current {sort_direction}" : nil
-css_class = column == sort_column ? "hilite" : nil
-#css_class = nil
-    link_to title, {:sort => column}, { :id => id, :class => css_class}
+#flash[:notice] = "sort_column: #{sort_column}"
+    link_to title, {:sort => column}, { :id => id }
   end
+
+  def css_class(title)
+    sort_column == title ? "hilite" : nil
+  end  
 end
